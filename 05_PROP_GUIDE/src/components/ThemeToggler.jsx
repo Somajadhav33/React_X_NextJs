2fcf36412d4c8571ef6599e8bd9ab42c9022167e
Soma <jadhavsomnath7005@gmail.com>
2025-12-12 12:53:41 +0530

    update: ThemeToggler.jsx

--- 05_PROP_GUIDE/src/components/ThemeToggler.jsx
+++ 05_PROP_GUIDE/src/components/ThemeToggler.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useState } from "react";
 
 // Create Theme Context
